3cb88e93253a9487a5448cc5b77759f8a9adda29
SwathiPriya37 <rswathipriya3@gmail.com>
2025-05-29 18:18:43 +0530

    modified jsx

--- ToDoList/src/components/ToDo.jsx
+++ ToDoList/src/components/ToDo.jsx
@@ -2,8 +2,13 @@ import './css/ToDoItems.css'
 
 const ToDo = () => {
   return (
-    <div>
-      
+    <div className='todo'>
+        <div className='todo-header'>To-DO List</div>
+      <div className="todo-add">
+        <input type="text" placeholder='Add your Task' className='todo-input'/>
+        <div classname='todo-add-btn'>ADD</div>  
+        <div className='todo-list'></div>
+      </div>
     </div>
   )
 }
