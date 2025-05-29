21538cb4ad7a8cca5ca70d962039575da42ead55
SwathiPriya37 <rswathipriya3@gmail.com>
2025-05-29 18:44:38 +0530

    added task items

--- ToDoList/src/components/ToDoItems.jsx
+++ ToDoList/src/components/ToDoItems.jsx
@@ -1,11 +1,28 @@
+import React from 'react';
 import './ToDoItems.css';
 
-const ToDoItems = () => {
+const ToDoItems = ({ task, onDelete, onToggle, completed }) => {
   return (
-    <div>
-      
+    <div className={`todo-item ${completed ? 'completed' : ''}`}>
+      <div className="todo-item-content">
+        <input 
+          type="checkbox" 
+          checked={completed}
+          onChange={onToggle}
+          className="todo-checkbox"
+        />
+        <span className="todo-text">
+          {task}
+        </span>
+      </div>
+      <button 
+        onClick={onDelete}
+        className="todo-delete-btn"
+      >
+        Delete
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default ToDoItems
+export default ToDoItems;
